2a2a4c786280a9865740d14dac7df9451395d9da
Karthik Meda <karthikreddymeda@gmail.com>
2017-05-15 21:17:37 +0530

    Geolocation data

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,17 +1,32 @@
 var expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
-describe("Generate message", ()=>{
+describe("generateMessage", ()=>{
+  it("Should generate correct message object", ()=>{
 
-it("Should generate correct message object", ()=>{
+      var from = "Admin";
+      var text = "Welcome to chat app";
+      var res = generateMessage(from, text);
 
-    var from = "Admin";
-    var text = "Welcome to chat app";
-    var res = generateMessage(from, text);
-
-    expect(res.createdAt).toBeA('number');
-    expect(res).toInclude({from, text});
+      expect(res.createdAt).toBeA('number');
+      expect(res).toInclude({from, text});
+  });
 });
+
+describe("generateLocationMessage", ()=>{
+
+  it("should generate correct location object", ()=>{
+
+    var from = 'Admin';
+    var latitude = 1;
+    var longitude = 1;
+    var url = `https://www.google.com/maps?q=1,1`;
+
+    var locObj = generateLocationMessage(from, latitude, longitude);
+
+    expect(locObj.createdAt).toBeA('number');
+    expect(locObj).toInclude({from,url});
+  });
 
 
 });
