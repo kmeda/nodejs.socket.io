e26fa78e722256091e4ef3440906de39829d0e12
Karthik Meda <karthikreddymeda@gmail.com>
2017-05-15 10:11:27 +0530

    express setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+
+
+const publicPath = path.join(__dirname + '../public');
+const port = process.env.PORT || 3000
+var app = express();
+
+app.use(express.static(publicPath));
+app.set('view-engine', 'html');
+
+app.get('/', (req, res)=>{
+  res.send('index');
+});
+
+app.listen(port, ()=>{
+  console.log(`Server started on port ${port}..`);
+});
