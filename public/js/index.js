16ec1cc638f8a9319bdeb6705fbb445b63f2d024
Karthik Meda <karthikreddymeda@gmail.com>
2017-05-16 11:25:32 +0530

    location error

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,20 @@
-
-
 var socket = io();
+
+function scrollToBottom(){
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+      messages.scrollTop(scrollHeight);
+    }
+};
+
 socket.on('connect', function(){
   console.log("Connected to server");
 });
@@ -20,12 +34,8 @@ socket.on('newMessage', function(message){
     from: message.from,
     createdAt: formattedTime
   });
-
   $('#messages').append(html);
-
-  // var li = $('<li></li>');
-  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  // $('#messages').append(li);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message){
@@ -38,6 +48,7 @@ socket.on('newLocationMessage', function(message){
     createdAt: formattedTime
   });
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 $('#message-form').on('submit', function(e){
